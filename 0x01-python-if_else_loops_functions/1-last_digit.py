3c8060da21e4727e9174adf61c9b52d74e0f254c
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-08 18:35:26 +0100

    Program that assigns random number and print the last integer of the number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+mod = 10
+
+if number < 0:
+    mod *= -1
+
+last_digit = number % mod
+
+if last_digit == 0:
+    str = "is 0"
+elif last_digit < 6 and last_digit != 0:
+    str = "is less than 6 and not 0"
+elif last_digit > 5:
+    str = "and is greater than 5"
+
+print(f"Last digit of {number} is {last_digit} and {str}")
