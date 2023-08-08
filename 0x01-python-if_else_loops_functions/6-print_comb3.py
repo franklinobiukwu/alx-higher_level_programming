b47ea5f19e076ebfd32b54bbb45fef6a93dda590
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-08 20:12:42 +0100

    program that prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(99):
+    if (((i%10) * 10) + (i//10) < i) or (i % 10) == (i // 10):
+        continue
+    elif i < 89:
+            print("{:02d}, ".format(i), end="")
+    else:
+            print("{:02d}".format(i))
