ff48f12c6e83434d129ecf7a85f0d46df62b8afd
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-08 05:47:47 +0100

    Used one write function

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,8 +2,7 @@
 
 import sys
 
-str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 
 sys.stdout.write(str)
-sys.stdout.write("\n")
-exit(1)
+sys.exit(1)
