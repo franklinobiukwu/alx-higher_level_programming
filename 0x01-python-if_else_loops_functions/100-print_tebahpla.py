fb0429701d773f6a3ae96161114c3d64ea9a41b2
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-09 16:02:50 +0100

    program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+count = 0
+
+for i in reversed(range(ord('a'), ord('z') + 1)):
+    count+=1
+    if (count % 2 == 0):
+        num = -32
+    else:
+        num = 0
+    print("{}".format(chr(i + num)), end="")
