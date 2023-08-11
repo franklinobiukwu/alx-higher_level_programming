6b5659844128ba615362dbf499951eb1f7c41881
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-11 18:07:33 +0100

    FIX # optimised algorithm

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,18 +7,12 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
-    if argv[2] not in ("+", "-", "*", "/"):
+    operations = {"+": add, "-": sub, "*": mul, "/":div}
+    if argv[2] not in list(operations.keys()):
         print(f"Uknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
 
-    if argv[2] == "+":
-        print(f"{a} {argv[2]} {b} = {add(a, b):}")
-    elif argv[2] == "-":
-        print(f"{a} {argv[2]} {b} = {sub(a, b):}")
-    elif argv[2] == "*":
-        print(f"{a} {argv[2]} {b} = {mul(a, b):}")
-    elif argv[2] == "/":
-        print(f"{a} {argv[2]} {b} = {div(a, b):}")
+    print(f"{a} {argv[2]} {b} = {operations[argv[2](a, b)}")
