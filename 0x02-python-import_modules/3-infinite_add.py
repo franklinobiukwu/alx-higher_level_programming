8a877173b625f736ab165335f96e17f4f552a787
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-11 03:40:14 +0100

    FIX #1: Made program to print 0 if no argument was provided

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     from sys import argv
 
-    sum = int(argv[1])
-    for i in range(2, len(argv)):
+    sum = 0
+    for i in range(1, len(argv)):
         sum += int(argv[i])
     print(sum)
