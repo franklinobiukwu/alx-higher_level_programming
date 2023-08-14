bc28faf24f05665c6a67111a43941d5ac29e66ee
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-14 01:44:20 +0100

    Return None if list is None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,7 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list == []:
         return
+    if my_list is None:
+        return None
     for item in list(reversed(my_list)):
         print("{:d}".format(item))
