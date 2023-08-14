e57303c948c50959596c332194778805ccd757b2
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-14 02:00:50 +0100

    Fix basd index access iddue

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
     if length == 0:
         first = None
+    else:
+        first = sentence[0]
     return ((length, first))
