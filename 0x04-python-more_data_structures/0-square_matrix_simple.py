c5fd2002b447900646e00fa78055081dcd369dca
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-16 21:31:23 +0100

    Fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in range(len(matrix)):
-        new_matrix.append(list(map(lambda x : x**2, matrix[i])))
+        new_matrix.append(list(map(lambda x: x**2, matrix[i])))
     return new_matrix
