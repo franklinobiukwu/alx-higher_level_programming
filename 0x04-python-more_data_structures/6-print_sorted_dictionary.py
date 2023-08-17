d193123e2cefb6a3401e3aa2954cfe1831619d60
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-17 15:46:21 +0100

    Made fuction print in correct format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    print(dict(sorted(a_dictionary.items())))
+    sorted_dict = sorted(a_dictionary.items())
+    for item in sorted_dict:
+        print(f"{item[0]}: {item[1]}")
