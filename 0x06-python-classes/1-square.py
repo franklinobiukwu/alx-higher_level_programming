b1057a9d7701950df9b700e17d0171f581f64c7b
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-30 05:42:56 +0100

    A class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size):
+        """initialize the square instances
+        Args:
+            size - size of defined square """
+        self.__size = size
