44636237ffea10c4896b0bc28a6314191f5715ca
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-12 00:47:34 +0100

    a class Rectangle that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that contains a class that raises an exception"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This is a class that raises an exception"""
+
+    def __init__(self, width, height):
+        """Initialize rectangle instances"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns area of a rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns rectangle description"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
