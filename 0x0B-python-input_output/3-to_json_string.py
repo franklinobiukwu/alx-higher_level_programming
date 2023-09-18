29e025d5ac117bc3b67ae977ebd14cbb75380198
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-18 10:49:30 +0100

    function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module of to_json_string - A function that returns
+    the JSON representation of an object (string)
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """A function that returns the JSON representation
+        of an object (string)
+
+        Args:
+            my_obj: object (string) to be converted to JSON
+
+        Returns:
+            JSON representation of my_obj
+    """
+    return json.dumps(my_obj)
