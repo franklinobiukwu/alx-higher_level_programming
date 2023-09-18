476065505aabb4ebcad5c3259ea37f05c1fb226a
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-18 10:31:02 +0100

    Added docstring for function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,14 @@
 
 
 def write_file(filename="", text=""):
+    """function that writes a string to a text file
+        and returns the number of characters written.
+
+        Args:
+            filename: name of file to be created or truncated
+            text: text to be written to file
+
+        Returns: number of characters written
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
