a9b6d7778b5b45b19891ff468f2f266c941c556b
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-18 16:00:16 +0100

    Added more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,6 +7,7 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     """Class to test the Rectangle class"""
+
     def test_rec_blank(self):
         """Instantiate rectangle without any value"""
         self.assertRaises(TypeError, Rectangle)
@@ -18,3 +19,26 @@ class TestRectangle(unittest.TestCase):
     def test_rec_oneStr(self):
         """Instantiate rectangle with one string"""
         self.assertRaises(TypeError, Rectangle, "hey")
+
+    def test_rec_getW(self):
+        """Test get width functionality of rectangle class"""
+        r = Rectangle(20, 10)
+        self.assertEqual(r.width, 20)
+
+    def test_rec_setW_int(self):
+        """Test get width functionality of rectangle class integer"""
+        r = Rectangle(20, 10)
+        r.width = 5
+        self.assertEqual(r.width, 5)
+
+    def test_rec_setW_str(self):
+        """Test get width functionality of rectangle class with string"""
+        r = Rectangle(20, 10)
+        r.width = "hey"
+        self.assertEqual(r.width, "hey")
+
+    def test_rec_setH_int(self):
+        """Test get width functionality of rectangle class"""
+        r = Rectangle(20, 10)
+        r.height = 15
+        self.assertEqual(r.height, 15)
