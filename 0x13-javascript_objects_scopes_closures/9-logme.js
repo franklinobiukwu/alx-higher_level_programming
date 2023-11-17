9ddd71c253d788acad787235a54baa83b7d21255
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-17 17:31:36 +0100

    a function that prints the number of arguments already printed and the new argument value

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*  a function that prints the number of arguments
+ *  already printed and the new argument value */
+
+let store = 0;
+
+exports.logMe = function (item) {
+	function logger () {
+		console.log(store, item)
+		store += 1;
+	}
+	logger()
+}
