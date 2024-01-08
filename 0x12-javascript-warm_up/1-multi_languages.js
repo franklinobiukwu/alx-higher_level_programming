41c4757a26e738f7ba31c8f813b135ea5ef390e0
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-09 00:31:45 +0100

    Script that prints 3 lines of strings

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/* Script that prints 3 lines:
+    * The first line: "C is fun"
+    * The second line: "Python is cool"
+    * The third line: "JavaScript is amazing"
+    * You must use console.log(...) to print all output
+    * You are not allowed to use var
+    * */
+
+const cVar = 'C is fun';
+const pythonVar = 'Python is cool';
+const jsVar = 'JavaScript is amazing';
+
+console.log(cVar);
+console.log(pythonVar);
+console.log(jsVar);
