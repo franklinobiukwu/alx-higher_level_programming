593b3b65df19403d622d9b5824773d230017625c
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-09 10:11:43 +0100

    Removed var keyword from comment

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 /* Write a script that prints two arguments passed to it,
     in the following format: “ is ”
     * You must use console.log(...) to print all output
-    * You are not allowed to use var */
+    * You are not allowed to use  */
 
 const passedArgument = process.argv.slice(2);
 
