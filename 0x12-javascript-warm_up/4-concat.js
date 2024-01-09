586a34775e9fecce9f6688eb4298a85891bfdd8a
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-09 01:28:46 +0100

    script that prints two arguments passed to it

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* Write a script that prints two arguments passed to it,
+    in the following format: “ is ”
+    * You must use console.log(...) to print all output
+    * You are not allowed to use var */
+
+const passedArgument = process.argv.slice(2);
+
+console.log(`${passedArgument[0]} is ${passedArgument[1]}`);
