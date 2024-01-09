9db4fb4e68edebae95363ba94cc7568cf31fd0a0
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-10 00:39:57 +0100

    script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* Write a script that prints the addition of 2 integers
+    * The first argument is the first integer
+    * The second argument is the second integer
+    * You have to define a function with this prototype: function add(a, b)
+    * You must use console.log(...) to print all output
+    * You are not allowed to use var */
+
+const addEm = function (a, b) {
+    console.log(a + b)
+}
+
+const args = process.argv.slice(2)
+
+if (args.length > 1) {
+    const a = parseInt(args[0])
+    const b = parseInt(args[1])
+
+    addEm(a, b)
+}
