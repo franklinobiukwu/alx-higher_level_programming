0f4e55862bc7a08268e664d34639306bbe3e42cb
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-09 01:07:21 +0100

    Script that prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+/*  Script that prints a message depending of the number of arguments passed:
+    *  If no arguments are passed to the script, print “No argument”
+    *  If only one argument is passed to the script, print “Argument found”
+    *  Otherwise, print “Arguments found”
+    *  You must use console.log(...) to print all output
+    *  You are not allowed to use var */
+
+// import the process module
+const { argv } = require('node:process');
+
+// set the command-line argument array in variable
+const cmdArgArray = argv;
+
+/* extract the arguments excluding the first two elements
+    * which are node and the script filename */
+const selectedCmdArray = cmdArgArray.slice(2);
+
+/* print "No argument" if no argument is passed
+    * print “Argument found” if only one argument is passed
+    * else print "Arguments found" */
+if (selectedCmdArray.length === 0) {
+  console.log('No argument');
+} else if (selectedCmdArray.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
