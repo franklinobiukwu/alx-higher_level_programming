8f7e08d36743df1151c0fb102ffde37dca9b1eec
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-10 01:57:35 +0100

    removed "var" keyword from comment

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -3,7 +3,7 @@
 /* Write a function that returns the addition of 2 integers.
     * The function must be visible from outside
     * The name of the function must be add
-    * You are not allowed to use var */
+    * You are not allowed to use */
 
 const add = function (a, b) {
   return (a + b);
