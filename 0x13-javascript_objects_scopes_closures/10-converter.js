421431d1d3759df98563c8e65eb598c7c3cb94fd
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-01-12 06:15:11 +0100

    removed "var" from comment

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -4,7 +4,7 @@
     *   to another base passed as argument:
     * Prototype: exports.converter = function (base)
     * You are not allowed to import any file
-    * You are not allowed to declare any new variable (var, let, etc..) */
+    * You are not allowed to declare any new variable ("var", let, etc..) */
 
 exports.converter = function (base) {
   const convert = function (number) {
